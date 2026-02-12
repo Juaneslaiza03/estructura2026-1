88a23066b4144c47fc839fc5b6440b18a868c08a
Juan Esteban Loaiza Lopez <jestebanlpez@gmail.com>
2026-02-11 21:12:34 -0500

    metodos

---
+++ Metodos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Metodos {
+
+    public ObjVehiculo[][] LlenarMatriz(ObjVehiculo[][] M){
+
+        Scanner sc= new Scanner(System.in);
+        
+            
+        
+        return M;
+
+    }
+    
+}
